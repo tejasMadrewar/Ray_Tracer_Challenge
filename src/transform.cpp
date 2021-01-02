ba8d6f3d51fa64b4e545d3cca67ca79f5a2056e1
Tejas Madrewar <tejas.madrewar@gmail.com>
2021-01-02 20:43:33 +0530

    transform update
    seperated transform declaration and defination

--- src/transform.cpp
+++ src/transform.cpp
@@ -1,1 +1,74 @@
 #include "./transform.h"
+
+mat transform::translate(float x, float y, float z) {
+  mat result(4);
+  result = result.Identity(4);
+  result.at(0, 3) = x;
+  result.at(1, 3) = y;
+  result.at(2, 3) = z;
+
+  return result;
+}
+
+mat transform::scale(float x, float y, float z) {
+  mat result(4);
+  result = result.Identity(4);
+  result.at(0, 0) = x;
+  result.at(1, 1) = y;
+  result.at(2, 2) = z;
+
+  return result;
+}
+
+mat transform::rotX(const float radian) {
+  mat result(4);
+  result = result.Identity(4);
+  result.at(0, 0) = 1;
+  result.at(1, 1) = cos(radian);
+  result.at(1, 2) = -sin(radian);
+  result.at(2, 1) = sin(radian);
+  result.at(2, 2) = cos(radian);
+  result.at(3, 3) = 1;
+
+  return result;
+}
+
+mat transform::rotY(const float radian) {
+  mat result(4);
+  result = result.Identity(4);
+  result.at(0, 0) = cos(radian);
+  result.at(0, 2) = sin(radian);
+  result.at(1, 1) = 1;
+  result.at(2, 0) = -sin(radian);
+  result.at(2, 2) = cos(radian);
+  result.at(3, 3) = 1;
+
+  return result;
+}
+
+mat transform::rotZ(const float radian) {
+  mat result(4);
+  result = result.Identity(4);
+  result.at(0, 0) = cos(radian);
+  result.at(0, 1) = -sin(radian);
+  result.at(1, 0) = sin(radian);
+  result.at(1, 1) = cos(radian);
+  result.at(2, 2) = 1;
+  result.at(3, 3) = 1;
+
+  return result;
+}
+
+mat transform::shear(const float xy, const float xz, const float yx,
+                     const float yz, const float zx, const float zy) {
+  mat result(4);
+  result = result.Identity(4);
+  result.at(0, 1) = xy;
+  result.at(0, 2) = xz;
+  result.at(1, 0) = yx;
+  result.at(1, 2) = yz;
+  result.at(2, 0) = zx;
+  result.at(2, 1) = zy;
+
+  return result;
+}
--- src/transform.h
+++ src/transform.h
@@ -11,80 +11,13 @@ class transform {
 public:
   transform() {}
   ~transform() {}
-
-  static mat translate(float x, float y, float z) {
-    mat result(4);
-    result = result.Identity(4);
-    result.at(0, 3) = x;
-    result.at(1, 3) = y;
-    result.at(2, 3) = z;
-
-    return result;
-  }
-
-  static mat scale(float x, float y, float z) {
-    mat result(4);
-    result = result.Identity(4);
-    result.at(0, 0) = x;
-    result.at(1, 1) = y;
-    result.at(2, 2) = z;
-
-    return result;
-  }
-
-  static mat rotX(const float radian) {
-    mat result(4);
-    result = result.Identity(4);
-    result.at(0, 0) = 1;
-    result.at(1, 1) = cos(radian);
-    result.at(1, 2) = -sin(radian);
-    result.at(2, 1) = sin(radian);
-    result.at(2, 2) = cos(radian);
-    result.at(3, 3) = 1;
-
-    return result;
-  }
-
-  static mat rotY(const float radian) {
-    mat result(4);
-    result = result.Identity(4);
-    result.at(0, 0) = cos(radian);
-    result.at(0, 2) = sin(radian);
-    result.at(1, 1) = 1;
-    result.at(2, 0) = -sin(radian);
-    result.at(2, 2) = cos(radian);
-    result.at(3, 3) = 1;
-
-    return result;
-  }
-
-  static mat rotZ(const float radian) {
-    mat result(4);
-    result = result.Identity(4);
-    result.at(0, 0) = cos(radian);
-    result.at(0, 1) = -sin(radian);
-    result.at(1, 0) = sin(radian);
-    result.at(1, 1) = cos(radian);
-    result.at(2, 2) = 1;
-    result.at(3, 3) = 1;
-
-    return result;
-  }
-
+  static mat translate(float x, float y, float z);
+  static mat scale(float x, float y, float z);
+  static mat rotX(const float radian);
+  static mat rotY(const float radian);
+  static mat rotZ(const float radian);
   static mat shear(const float xy, const float xz, const float yx,
-                   const float yz, const float zx, const float zy) {
-    mat result(4);
-    result = result.Identity(4);
-    result.at(0, 1) = xy;
-    result.at(0, 2) = xz;
-    result.at(1, 0) = yx;
-    result.at(1, 2) = yz;
-    result.at(2, 0) = zx;
-    result.at(2, 1) = zy;
-
-    return result;
-  }
-
+                   const float yz, const float zx, const float zy);
 private:
 };
 
